ef33203a7c5aa0689b774d1620230461a178fe45
USER <USER@MSI>
2017-04-26 16:12:12 +0800

    2017.04.26.v3

--- src/cc/openhome/JNotePad.java
+++ src/cc/openhome/JNotePad.java
@@ -3,6 +3,10 @@ package cc.openhome;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.swing.*;
 
 public class JNotePad extends JFrame { // 繼承JFrame
@@ -24,7 +28,16 @@ public class JNotePad extends JFrame { // 繼承JFrame
 	private JTextArea textArea;
 	private JLabel stateBar;
 
-	private JPopupMenu popUpMenu;
+	private JPopupMenu popUpMenu; // 出現視窗
+
+	private JFileChooser fileChooser;
+
+	private TextDAO textDAO;
+
+	public JNotePad(TextDAO textDAO) {
+		this();
+		this.textDAO = textDAO;
+	}
 
 	public JNotePad() {
 		initComponents(); // 初始元件外觀
@@ -41,6 +54,8 @@ public class JNotePad extends JFrame { // 繼承JFrame
 		initTextArea();
 		initStateBar();
 		popUpMenu = editMenu.getPopupMenu();
+		fileChooser = new JFileChooser();
+
 	}
 
 	private void initStateBar() {
@@ -146,24 +161,21 @@ public class JNotePad extends JFrame { // 繼承JFrame
 			}
 		});
 	}
-	
-    private void initMenuListener() {
-        menuOpen.addActionListener(this::openFile); // 選單 - 開啟舊檔
-        menuSave.addActionListener(this::saveFile); // 選單 - 儲存檔案
-        menuSaveAs.addActionListener(this::saveFileAs); // 選單 - 另存新檔
-        menuClose.addActionListener(this::closeFile); // 選單 - 關閉檔案
-        menuCut.addActionListener(this::cut); // 選單 - 剪下
-        menuCopy.addActionListener(this::copy); // 選單 - 複製
-        menuPaste.addActionListener(this::paste); // 選單 - 貼上
-        menuAbout.addActionListener(event -> { // 選單 - 關於
-            JOptionPane.showOptionDialog(null,  // 顯示對話方塊
-                    "JNotePad 0.1\n來自 http://openhome.cc",
-                    "關於JNotePad",
-                    JOptionPane.DEFAULT_OPTION,
-                    JOptionPane.INFORMATION_MESSAGE,
-                    null, null, null);
-        });
-    }
+
+	private void initMenuListener() {
+		menuOpen.addActionListener(this::openFile); // 選單 - 開啟舊檔
+		menuSave.addActionListener(this::saveFile); // 選單 - 儲存檔案
+		menuSaveAs.addActionListener(this::saveFileAs); // 選單 - 另存新檔
+		menuClose.addActionListener(this::closeFile); // 選單 - 關閉檔案
+		menuCut.addActionListener(this::cut); // 選單 - 剪下
+		menuCopy.addActionListener(this::copy); // 選單 - 複製
+		menuPaste.addActionListener(this::paste); // 選單 - 貼上
+		menuAbout.addActionListener(event -> { // 選單 - 關於
+			JOptionPane.showOptionDialog(null, // 顯示對話方塊
+					"JNotePad 0.1\n來自 http://openhome.cc", "關於JNotePad", JOptionPane.DEFAULT_OPTION,
+					JOptionPane.INFORMATION_MESSAGE, null, null, null);
+		});
+	}
 
 	private void initAccelerator() {
 		// 快速鍵設置
@@ -175,20 +187,114 @@ public class JNotePad extends JFrame { // 繼承JFrame
 		menuPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK)); // 貼上Ctrl+V
 
 	}
-	
-    private void closeWindow(WindowEvent event) {}
-    private void openFile(ActionEvent event) {}
-    private void saveFile(ActionEvent event) {}    
-    private void saveFileAs(ActionEvent event) {}    
-    private void closeFile(ActionEvent event) {}
-    private void cut(ActionEvent event) {}
-    private void copy(ActionEvent event) {}    
-    private void paste(ActionEvent event) {} 
-    private void jtextAreaActionPerformed(KeyEvent event) {}
+
+	private void openFile(ActionEvent event) {
+		if (stateBar.getText().equals("未修改")) { // 文件是否為儲存狀態
+			showFileDialog(); // 開啟舊檔
+		} else {
+			int option = JOptionPane.showInternalConfirmDialog( // 顯示對話方塊
+					null, "檔案已修改，是否儲存?", "儲存檔案?", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null);
+			switch (option) {
+			case JOptionPane.YES_OPTION: // 確認儲存檔案
+				saveFile(event); // 儲存檔案
+				break;
+			case JOptionPane.NO_OPTION: // 放棄檔案儲存
+				showFileDialog();
+				break;
+			}
+		}
+	}
+
+	private void showFileDialog() {
+		int option = fileChooser.showDialog(null, null); // 檔案選取對話方塊
+
+		// 使用者按下確認鍵
+		if (option == JFileChooser.APPROVE_OPTION) {
+			try {
+				setTitle(fileChooser.getSelectedFile().toString()); // 設定文件標題
+				textArea.setText(""); // 清除前一次文件
+				stateBar.setText("未修改"); // 設定狀態列
+				String text = textDAO.read(fileChooser.getSelectedFile().toString()); // 讀取檔案
+				textArea.setText(text); // 附加至文字編輯區
+			} catch (Throwable e) {
+				JOptionPane.showMessageDialog(null, e.toString(), "開啟檔案失敗", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	private void saveFile(ActionEvent event) { // 儲存檔案
+		// 從標題列取得檔案名稱
+		Path path = Paths.get(getTitle());
+		if (Files.notExists(path)) { // 若指定的檔案不存在
+			saveFileAs(event); // 執行令存新檔
+		} else {
+			try {
+				// 儲存檔案
+				textDAO.save(path.toString(), textArea.getText());
+				// 設定狀態列為未修改
+				stateBar.setText("未修改");
+			} catch (Throwable e) {
+				JOptionPane.showMessageDialog(null, e.toString(), "寫入檔案失敗", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	private void saveFileAs(ActionEvent event) { // 另存新檔
+		// 顯示檔案對話方塊
+		int option = fileChooser.showDialog(null, null);
+		// 如果選取檔案
+		if (option == JFileChooser.APPROVE_OPTION) {
+			// 在標題列上設定檔案名稱
+			setTitle(fileChooser.getSelectedFile().toString());
+			textDAO.create(fileChooser.getSelectedFile().toString()); // 建立檔案
+			saveFile(event); // 進行儲存檔案
+		}
+	}
+
+	private void closeWindow(WindowEvent event) {
+		closeFile(new ActionEvent(event.getSource(), event.getID(), "windowClosing"));
+	}
+
+	private void closeFile(ActionEvent event) {
+		if (stateBar.getText().equals("未修改")) { // 是否已儲存文件
+			dispose(); // 釋放視窗資源，關閉程式
+		} else {
+			int option = JOptionPane.showConfirmDialog(null, "檔案已修改，是否儲存?", "儲存檔案?", JOptionPane.YES_NO_OPTION,
+					JOptionPane.WARNING_MESSAGE, null);
+			switch (option) {
+			case JOptionPane.YES_OPTION:
+				saveFile(event);
+				break;
+			case JOptionPane.NO_OPTION:
+				dispose();
+			}
+		}
+	}
+
+	private void cut(ActionEvent event) { // 剪下
+        textArea.cut();
+        stateBar.setText("已修改");
+        popUpMenu.setVisible(false);
+	}
+
+	private void copy(ActionEvent event) { // 複製
+        textArea.copy();
+        popUpMenu.setVisible(false); 
+	}
+
+	private void paste(ActionEvent event) { // 貼上
+        textArea.paste();
+        stateBar.setText("已修改");
+        popUpMenu.setVisible(false);
+	}
+
+	private void jtextAreaActionPerformed(KeyEvent event) {
+		stateBar.setText("已修改");
+	}
 
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(() -> { // 將建立JNotePad實例與
-			new JNotePad().setVisible(true); // setVistble()的動作排入事件佇列
+			new JNotePad(new FileTextDAO()).setVisible(true); // setVistble()的動作排入事件佇列
 		});
 	}
 }
